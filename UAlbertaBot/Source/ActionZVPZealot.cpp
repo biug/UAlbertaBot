29be4485e01e43db19ee78fcdaae1cb5762a74bc
cpus133525 <365248477@qq.com>
2017-07-11 17:03:28 +0800

    添加ZVP神族战术2

--- UAlbertaBot/Source/ActionZVPZealot.cpp
+++ UAlbertaBot/Source/ActionZVPZealot.cpp
@@ -1,1 +1,209 @@
 #include "ActionZVPZealot.h"
+#include "BuildingManager.h"
+#include "ActionHelper.h"
+#include "InformationManager.h"
+
+using namespace CasiaBot;
+using namespace UAlbertaBot;
+using namespace CasiaBot::ActionHelper;
+using namespace std;
+
+ActionZVPZealot::ActionZVPZealot()
+{
+	init();
+}
+
+void ActionZVPZealot::init()
+{
+	if (!isInitialized)
+	{
+		updateCurrentState();
+		mineralNetIncrease = { 0,0,0,0,0 };
+		gasNetIncrease = { 0,0,0,0,0 };
+		lastFrameCount = BWAPI::Broodwar->getFrameCount();
+		lastFrameMineralAmount = BWAPI::Broodwar->self()->minerals();
+		lastFrameGasAmount = BWAPI::Broodwar->self()->gas();
+		isInitialized = true;
+	}
+}
+
+bool ActionZVPZealot::canDeployAction()
+{
+	updateState();
+	if (enemyDragoonOverZealotRate <= 1 && enemy_dragoon_count <= 5)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool ActionZVPZealot::tick()
+{
+	updateState();
+	if (enemyDragoonOverZealotRate > 1 || enemy_dragoon_count > 5)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void ActionZVPZealot::getBuildOrderList(UAlbertaBot::ProductionQueue & queue)
+{
+	updateState();
+
+	// 判断是否需要增加母巢
+	int currentFrameCount = BWAPI::Broodwar->getFrameCount();
+	if (hatch_count <= 4 && currentFrameCount && currentFrameCount % 200 == 0)
+	{
+		int currentFrameMineralAmount = BWAPI::Broodwar->self()->minerals();
+		int currentFrameGasAmount = BWAPI::Broodwar->self()->gas();
+		int diffMineralAmount = currentFrameMineralAmount - lastFrameMineralAmount;
+		int diffGasAmount = currentFrameGasAmount - lastFrameGasAmount;
+
+		mineralNetIncrease.pop_front();
+		mineralNetIncrease.push_back(diffMineralAmount);
+		gasNetIncrease.pop_front();
+		gasNetIncrease.push_back(diffGasAmount);
+
+		bool mineralDequePositive;
+		bool gasDequePositive;
+
+		if (hatch_count <= 2)
+		{
+			mineralDequePositive = IsDequeAllPositive(mineralNetIncrease);
+			if (mineralDequePositive)
+			{
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Hatchery), true);
+			}
+		}
+		else
+		{
+			mineralDequePositive = IsDequeAllPositive(mineralNetIncrease);
+			gasDequePositive = IsDequeAllPositive(gasNetIncrease);
+			if (mineralDequePositive && gasDequePositive)
+			{
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Hatchery), true);
+			}
+		}
+
+		lastFrameCount = currentFrameCount;
+		lastFrameMineralAmount = currentFrameMineralAmount;
+		lastFrameGasAmount = currentFrameGasAmount;
+	}
+
+	// 判断前提建筑是否存在
+	bool isExtractorExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Extractor);
+	if (!isExtractorExist)
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Extractor), true);
+	}
+
+	bool isSpawningPoolExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Spawning_Pool);
+	if (!isSpawningPoolExist)
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Spawning_Pool), true);
+	}
+
+	bool isHiveExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Hive);
+	bool isQueenNestExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Queens_Nest);
+	bool isLairExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Lair);
+	if (!isHiveExist)
+	{
+		if (!isQueenNestExist)
+		{
+			if (!isLairExist)
+			{
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Lair), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Queens_Nest), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Hive), true);
+			}
+			else
+			{
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Queens_Nest), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Hive), true);
+			}
+		}
+		else
+		{
+			queue.add(MetaType(BWAPI::UnitTypes::Zerg_Hive), true);
+		}
+	}
+
+	bool isSpireExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Spire);
+	if (!isSpireExist)	// 若飞龙塔不存在
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Spire), true);
+	}
+
+	queue.add(MetaType(BWAPI::UpgradeTypes::Metabolic_Boost), true);
+	queue.add(MetaType(BWAPI::UpgradeTypes::Adrenal_Glands), true);
+
+	// 判断人口是否接近满，系统内部的supply数值是双倍的
+	if (BWAPI::Broodwar->self()->supplyUsed() + 8 >= BWAPI::Broodwar->self()->supplyTotal())
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Overlord), true);
+	}
+
+	// 判断需要建造多少部队
+	int need_zergling_count = enemy_zealot_count * 8 + enemy_dragoon_count * 7 - zergling_count;
+	int need_mutalisk_count = need_zergling_count / 4;
+
+	// 穿插建造Zergling和Mutalisk
+	do
+	{
+		if (need_zergling_count > 0)
+		{
+			// 2个Zergling
+			queue.add(MetaType(BWAPI::UnitTypes::Zerg_Zergling));
+			queue.add(MetaType(BWAPI::UnitTypes::Zerg_Zergling));
+			need_zergling_count -= 4;
+		}
+		if (need_mutalisk_count > 0)
+		{
+			queue.add(MetaType(BWAPI::UnitTypes::Zerg_Mutalisk));
+			need_mutalisk_count--;
+		}
+		if (need_zergling_count <= 0 && need_mutalisk_count <= 0)
+		{
+			break;
+		}
+
+	} while (true);
+
+	int extractorUpperBound = std::min(hatch_count, 3);
+	int currentExtractorCount = (int)InformationManager::Instance().getNumUnits(BWAPI::UnitTypes::Zerg_Extractor, BWAPI::Broodwar->self());
+	if (currentExtractorCount < extractorUpperBound)
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Extractor), true);
+	}
+}
+
+void ActionZVPZealot::updateState()
+{
+	updateCurrentState();
+
+	auto &info = InformationManager::Instance();
+	auto self = BWAPI::Broodwar->self();
+	auto enemy = BWAPI::Broodwar->enemy();
+	hatch_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Hatchery, self) + info.getNumUnits(BWAPI::UnitTypes::Zerg_Lair, self) + info.getNumUnits(BWAPI::UnitTypes::Zerg_Hive, self);
+	zergling_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Zergling, self);
+	mutalisk_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Mutalisk, self);
+	enemy_zealot_count = info.getNumUnits(BWAPI::UnitTypes::Protoss_Zealot, enemy);
+	enemy_dragoon_count = info.getNumUnits(BWAPI::UnitTypes::Protoss_Dragoon, enemy);
+	enemy_ht_count = info.getNumUnits(BWAPI::UnitTypes::Protoss_High_Templar, enemy);
+	enemy_dt_count = info.getNumUnits(BWAPI::UnitTypes::Protoss_Dark_Templar, enemy);
+	enemyDragoonOverZealotRate = (double)enemy_dragoon_count / enemy_zealot_count;
+}
