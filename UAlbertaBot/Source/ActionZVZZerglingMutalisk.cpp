e18639f3e520bd31e08b93384180eacc4964e827
cpus133525 <365248477@qq.com>
2017-07-11 13:05:43 +0800

    完成ZVZ战术之一

--- UAlbertaBot/Source/ActionZVZZerglingMutalisk.cpp
+++ UAlbertaBot/Source/ActionZVZZerglingMutalisk.cpp
@@ -29,16 +29,144 @@ void ActionZVZZerglingMutalisk::init()
 
 bool ActionZVZZerglingMutalisk::canDeployAction()
 {
-	return false;
+	updateState();
+	if (enemy_mutalisk_count > 0 || (enemy_hydralisk_count == 0 && enemy_mutalisk_count == 0))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 bool ActionZVZZerglingMutalisk::tick()
 {
-	return false;
+	updateState();
+	if (enemy_hydralisk_count > 0 && enemy_mutalisk_count == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 void ActionZVZZerglingMutalisk::getBuildOrderList(UAlbertaBot::ProductionQueue & queue)
 {
+	updateState();
+
+	// 判断是否需要增加母巢
+	int currentFrameCount = BWAPI::Broodwar->getFrameCount();
+	if (hatch_count <= 4 && currentFrameCount && currentFrameCount % 200 == 0)
+	{
+		int currentFrameMineralAmount = BWAPI::Broodwar->self()->minerals();
+		int currentFrameGasAmount = BWAPI::Broodwar->self()->gas();
+		int diffMineralAmount = currentFrameMineralAmount - lastFrameMineralAmount;
+		int diffGasAmount = currentFrameGasAmount - lastFrameGasAmount;
+
+		mineralNetIncrease.pop_front();
+		mineralNetIncrease.push_back(diffMineralAmount);
+		gasNetIncrease.pop_front();
+		gasNetIncrease.push_back(diffGasAmount);
+
+		bool mineralDequePositive;
+		bool gasDequePositive;
+
+		if (hatch_count <= 2)
+		{
+			mineralDequePositive = IsDequeAllPositive(mineralNetIncrease);
+			if (mineralDequePositive)
+			{
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Hatchery), true);
+			}
+		}
+		else
+		{
+			mineralDequePositive = IsDequeAllPositive(mineralNetIncrease);
+			gasDequePositive = IsDequeAllPositive(gasNetIncrease);
+			if (mineralDequePositive && gasDequePositive)
+			{
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+				queue.add(MetaType(BWAPI::UnitTypes::Zerg_Hatchery), true);
+			}
+		}
+
+		lastFrameCount = currentFrameCount;
+		lastFrameMineralAmount = currentFrameMineralAmount;
+		lastFrameGasAmount = currentFrameGasAmount;
+	}
+
+	// 判断前提建筑是否存在
+	bool isExtractorExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Extractor);
+	if (!isExtractorExist)
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Extractor), true);
+	}
+
+	bool isSpawningPoolExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Spawning_Pool);
+	if (!isSpawningPoolExist)
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Spawning_Pool), true);
+	}
+
+	bool isLairExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Lair);
+	if (!isLairExist)
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Lair), true);
+	}
+
+	bool isSpireExist = BuildingManager::Instance().isBeingBuilt(BWAPI::UnitTypes::Zerg_Spire);
+	if (!isSpireExist)	// 若飞龙塔不存在
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Drone), true);
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Spire), true);
+	}
+
+	queue.add(MetaType(BWAPI::UpgradeTypes::Metabolic_Boost), true);
+	queue.add(MetaType(BWAPI::UpgradeTypes::Adrenal_Glands), true);
+
+	// 判断人口是否接近满，系统内部的supply数值是双倍的
+	if (BWAPI::Broodwar->self()->supplyUsed() + 8 >= BWAPI::Broodwar->self()->supplyTotal())
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Overlord), true);
+	}
+
+	// 判断需要建造多少部队
+	int need_zergling_count = (int)(enemy_zergling_count * 1.5) - zergling_count;
+	int need_mutalisk_count = (int)(enemy_zergling_count*0.5) + enemy_mutalisk_count + enemy_hydralisk_count;
+
+	// 穿插建造Zergling和Mutalisk
+	do
+	{
+		if (need_zergling_count > 0)
+		{
+			// 2个Zergling
+			queue.add(MetaType(BWAPI::UnitTypes::Zerg_Zergling));
+			need_zergling_count -= 2;
+		}
+		if (need_mutalisk_count > 0)
+		{
+			queue.add(MetaType(BWAPI::UnitTypes::Zerg_Mutalisk));
+			need_mutalisk_count--;
+		}
+		if (need_zergling_count <= 0 && need_mutalisk_count <= 0)
+		{
+			break;
+		}
+
+	} while (true);
+
+	int extractorUpperBound = std::min(hatch_count, 2);
+	int currentExtractorCount = (int)InformationManager::Instance().getNumUnits(BWAPI::UnitTypes::Zerg_Extractor, BWAPI::Broodwar->self());
+	if (currentExtractorCount < extractorUpperBound)
+	{
+		queue.add(MetaType(BWAPI::UnitTypes::Zerg_Extractor), true);
+	}
 }
 
 void ActionZVZZerglingMutalisk::updateState()
@@ -51,4 +179,7 @@ void ActionZVZZerglingMutalisk::updateState()
 	hatch_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Hatchery, self) + info.getNumUnits(BWAPI::UnitTypes::Zerg_Lair, self) + info.getNumUnits(BWAPI::UnitTypes::Zerg_Hive, self);
 	zergling_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Zergling, self);
 	mutalisk_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Mutalisk, self);
+	enemy_zergling_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Zergling, enemy);
+	enemy_hydralisk_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Hydralisk, enemy);
+	enemy_mutalisk_count = info.getNumUnits(BWAPI::UnitTypes::Zerg_Mutalisk, enemy);
 }
--- UAlbertaBot/Source/ActionZergBase.h
+++ UAlbertaBot/Source/ActionZergBase.h
@@ -58,38 +58,38 @@ namespace CasiaBot
 		int enemy_zerg_unit_count;
 
 		//人族单位
-		int enemy_marine_count;				//机枪兵
+		int enemy_marine_count;					//机枪兵
 		int enemy_firebat_count;				//火焰兵
 		int enemy_medic_count;					//医疗兵
 		int enemy_ghost_count;					//幽灵
 		int enemy_vulture_count;				//雷车
 		int enemy_tank_count;					//坦克
 		int enemy_goliath_count;				//机器人
-		int enemy_wraith_count;				//隐飞
+		int enemy_wraith_count;					//隐飞
 		int enemy_valkyrie_count;				//导弹护卫舰
-		int enemy_bc_count;					//战列巡洋舰
-		int enemy_science_vessel_count;		//科学球
+		int enemy_bc_count;						//战列巡洋舰
+		int enemy_science_vessel_count;			//科学球
 		int enemy_dropship_count;				//运输机
 
 		//人族建筑
-		int enemy_bunker_count;				//地堡
+		int enemy_bunker_count;					//地堡
 		int enemy_barrack_count;				//兵营
 		int enemy_factory_count;				//工厂
 		int enemy_starport_count;				//飞机场
 
 		//神族单位
-		int enemy_zealot_count;				//狂热者
+		int enemy_zealot_count;					//狂热者
 		int enemy_dragoon_count;				//龙骑
-		int enemy_ht_count;					//光明圣堂
-		int enemy_dt_count;					//黑暗圣堂
-		int enemy_reaver_count;				//金甲虫
+		int enemy_ht_count;						//光明圣堂
+		int enemy_dt_count;						//黑暗圣堂
+		int enemy_reaver_count;					//金甲虫
 		int enemy_shuttle_count;				//运输机
 		int enemy_carrier_count;				//航母
 		int enemy_arbiter_count;				//仲裁者
 		int enemy_corsair_count;				//海盗船
 
 		//神族建筑
-		int enemy_cannon_count;				//光子炮塔
+		int enemy_cannon_count;					//光子炮塔
 		int enemy_gateway_count;				//兵营
 		int enemy_stargate_count;				//星门
 		int enemy_robotics_facility_count;		//机械工厂
@@ -97,7 +97,7 @@ namespace CasiaBot
 		//虫族单位
 		int enemy_zergling_count;				//小狗
 		int enemy_hydralisk_count;				//刺蛇
-		int enemy_lurker_count;				//地刺
+		int enemy_lurker_count;					//地刺
 		int enemy_ultralisk_count;				//雷兽
 		int enemy_defiler_count;				//蝎子
 		int enemy_mutalisk_count;				//飞龙
